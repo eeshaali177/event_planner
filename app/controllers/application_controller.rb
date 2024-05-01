e6e88009c8995043d57794d2e85e20942619f64c
eesha ali <eeshaali177@gmail.com>
2024-05-01 19:56:53 +0500

    2nd commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    protected
+
+    def require_no_authentication
+      if current_user
+        redirect_to root_path
+      end
+    end
 end
